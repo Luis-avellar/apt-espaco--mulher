84e5a00dd43cb6a14e40081368d6fd711b0aa99b
Luis Avellar <avellar.mr@gmail.com>
2025-11-11 13:34:57 -0300

    create nav structure and layout

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,24 @@
-const App = () => {}
+const App = () => (
+	<nav>
+		<div className='container'>
+			<img src='/img/logo-espaco-mulher.png' alt='logo do apt' />
+			<h1>Espaço Mulher</h1>
+		</div>
+		<div className='container-form'>
+			<form action='#'>
+				<label htmlFor='#'>
+					O que você precisa guardar?
+					<select>
+						<option value='1'>1</option>
+						<option value='2'>2</option>
+						<option value='3'>3</option>
+					</select>
+				</label>
+				<input type='text' placeholder='Mande aqui' />
+				<button>Adicionar</button>
+			</form>
+		</div>
+	</nav>
+)
 
 export { App }
