a250242b7af4d7be4c7b921464ec4bd9b1994df6
Luis Avellar <avellar.mr@gmail.com>
2025-11-12 15:04:44 -0300

    create feature for add grid-item

--- src/app.jsx
+++ src/app.jsx
@@ -1,58 +1,80 @@
 import { FaXmark } from 'react-icons/fa6'
+import { useState } from 'react'
 
-const App = () => (
-	<>
-		<nav>
-			<div className='container'>
-				<img src='/img/logo-espaco-mulher.png' alt='logo do apt' />
-				<h1>Espaço Mulher</h1>
-			</div>
+const App = () => {
+	const [product, setProduct] = useState([])
 
-			<div className='container-form'>
-				<form action='#'>
-					<label htmlFor='#'>
-						O que você precisa guardar?
-						<select>
-							<option value='1'>1</option>
-							<option value='2'>2</option>
-							<option value='3'>3</option>
+	const handleSubmit = e => {
+		e.preventDefault()
+
+		const { quantity, category } = e.target.elements
+
+		const newProduct = {
+			id: crypto.randomUUID(),
+			[quantity.name]: quantity.value,
+			[category.name]: category.value,
+		}
+
+		setProduct(prev => [...prev, newProduct])
+	}
+
+	return (
+		<>
+			<nav>
+				<div className='container'>
+					<img src='/img/logo-espaco-mulher.png' alt='logo do apt' />
+					<h1>Espaço Mulher</h1>
+				</div>
+
+				<div className='container-form'>
+					<form onSubmit={handleSubmit}>
+						<label htmlFor='#'>
+							O que você precisa guardar?
+							<select name='quantity'>
+								<option value='1'>1</option>
+								<option value='2'>2</option>
+								<option value='3'>3</option>
+							</select>
+						</label>
+						<input type='text' name='category' placeholder='Mande aqui' />
+						<button>Adicionar</button>
+					</form>
+				</div>
+			</nav>
+
+			<section>
+				<div className='container-grid'>
+					<div className='grid-item'>
+						{product.map(product => {
+							return (
+								<div className='item' key={product.id}>
+									<input type='checkbox' name='first' />
+									<span>{`${product.quantity} ${product.category}`}</span>
+									<FaXmark style={{ color: 'red' }} />
+								</div>
+							)
+						})}
+					</div>
+				</div>
+				<div className='container-btn'>
+					<form className='clear'>
+						<select name='opcoes'>
+							<option value='ordenar'>Ordenar pelo mais recentes</option>
+							<option value='guardados'>Mostrar só itens guardados</option>
+							<option value='ordem'>Ordem alfabética</option>
 						</select>
-					</label>
-					<input type='text' placeholder='Mande aqui' />
-					<button>Adicionar</button>
-				</form>
-			</div>
-		</nav>
+						<button>Limpar lista</button>
+					</form>
+				</div>
+			</section>
 
-		<section>
-			<div className='container-grid'>
-				<form className='grid'>
-					<label htmlFor='#'>
-						<div className='icon'>
-							<input type='checkbox' name='first' />4 Halteres
-							<FaXmark style={{ color: 'red' }} />
-						</div>
-					</label>
-				</form>
-			</div>
-			<div className='container-btn'>
-				<form className='clear'>
-					<select name='opcoes'>
-						<option value='ordenar'>Ordenar pelo mais recentes</option>
-						<option value='guardados'>Mostrar só itens guardados</option>
-						<option value='ordem'>Ordem alfabética</option>
-					</select>
-					<button>Limpar lista</button>
-				</form>
-			</div>
-		</section>
-
-		<footer>
-			<div className='container-form'>
-				<p>Você tem 0 itens na lista</p>
-			</div>
-		</footer>
-	</>
-)
+			<footer>
+				<div className='container-form'>
+					<p>Você tem 0 itens na lista</p>
+				</div>
+			</footer>
+		</>
+	)
+}
 
 export { App }
